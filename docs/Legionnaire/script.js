6b9f996b3058eedb61a3716a2c18e0e5f5e4d24b
MyNameIsZach <zaarroyo@packer.edu>
2018-04-24 10:42:36 -0400

    HELLO DAVIS

--- docs/Legionnaire/script.js
+++ docs/Legionnaire/script.js
@@ -1,5 +1,4 @@
 document.getElementById("buttonEncode").addEventListener("click", encode);
-
 var buttonBOI = 0;
 
 function encode() {
@@ -43,12 +42,3 @@ function encode() {
 	  document.getElementById(buttonBOI).innerHTML = arr.join("");
 	  buttonBOI ++;
 }
-
-/* Getting the ASCII value from a character in a string:
-  var x = "Hello World";        Start with a string variable
-  var xVal = x.charCodeAt(0)   Call the charCodeAt method on the string object
-                                The parameter it takes is a position in the string
-                                e.g. 0 is H, 1 is e, 2 is l, etc
-                                It will return the ASCII value of the character in that position
-  console.log(xVal)             Prints: 72 to the console
-*/
